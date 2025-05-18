abb1e8f55f6123c03a0cf20f54cf91ab87a04c22
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-05-18 21:23:40 +0100

    Set up react development environment and made my first react app

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>First REACT App YIPPEEE</h1>
+        <p>This is my first react app it was easier to set up than i thought it would be</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
