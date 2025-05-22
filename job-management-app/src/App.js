e0354bac9934bb736a68bac408f52acf0ca90801
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-05-22 15:21:03 +0100

    Created basic job form with css classes for job management app

---
+++ job-management-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import JobForm from './jobForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <JobForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
