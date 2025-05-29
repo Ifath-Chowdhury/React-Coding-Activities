811ba34c5fe8d18b7169b5e5fdef73b0548b6f53
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-05-29 20:10:05 +0100

    Did set up for a movie review app

---
+++ movie-review-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+      <nav>
+        <a>Home</a>
+        <a>Movies</a>
+        <a>Actors</a>
+      </nav>
+
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ movie-review-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className='main-section'>
+            <h1>Movie Review App</h1>
+
+            <p>
+                This is the landing page for the movie review app
+            </p>
+        </div>
+    )
+}
+
+export default Header;
